641294256d1693f57efc6ef49c7117cabcdd80bc
slavarobotam <byzalova@gmail.com>
2019-08-22 22:23:30 +0300

    My Django Girls app, 3 commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,12 @@
-from django.conf.urls import url
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.post_list, name='post_list'),
+# ]
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail')
+    path('', views.post_list, name='post_list'),
 ]
