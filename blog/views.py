0bd730ec737a6a35ea40878445defc22e065794f
slavarobotam <byzalova@gmail.com>
2019-08-22 23:41:46 +0300

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,15 @@
 # def post_detail(request, pk):
 #     post = get_object_or_404(Post, pk=pk)
 #     return render(request, 'blog/post_detail.html', {'post': post})
-
+#####
+# from django.shortcuts import render
+# def post_list(request):
+#     return render(request, 'blog/post_list.html', {})
+####
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
