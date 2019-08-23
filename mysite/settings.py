5c93df745a7461d85414497476722c19405239ac
slavarobotam <byzalova@gmail.com>
2019-08-23 19:23:14 +0300

    dirs

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,4 +122,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = "/home/ott45/my-first-blog/static"
 
-STATICFILES_DIRS = ( os.path.join('static'), )
+STATICFILES_DIRS = ( os.path.join('static'),
+                    /home/ott45/my-first-blog/static/css/,
+                    /home/ott45/my-first-blog/static/ )
