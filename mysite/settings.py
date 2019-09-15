6ef891ebdfeac7cc583b751bcb5920c17e25fd5b
mari <zefiratakuet@yandex.ru>
2019-09-15 10:58:26 +0300

    changed list order by date: order_by(-published_date)

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,17 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-#STATIC_ROOT = "/home/ott45/my-first-blog/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "blog"),
-    #'/home/ott45/my-first-blog/static/css/',
-    #'/home/ott45/my-first-blog/static/admin/css/',
-    #'/home/ott45/my-first-blog/blog/static/css/'
-    #
-
 ]
